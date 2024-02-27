28f3a8536e3dfa1cefa87bc38422e4dfbd007f2e
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-27 08:42:26 -0800

    1

--- main.c
+++ main.c
@@ -1,19 +1,62 @@
 #include "main.h"
 /**
-* _memset -  function fills the first n bytes of the memory area pointed
-* @s: array
-* @b: value
-* @n: number of bytes
-* Return: value of *s
+* main -  simple shell
+* Return: 0 on EOF
 */
-int main(int argc, char **argv)
+int main(void)
 {
+	int i;
+
+	char *buf = malloc(MAX_SIZE * sizeof(char));
+
+	int j;
+
 	printf("$ ");
 	fflush(stdout);
-	int i = 0;
-	while (argv[i] != '\n')
+	while (1)
 	{
+		char *token;
 
+		char **argv = malloc(MAX_WORDS * sizeof(char *));
+
+	       if (fgets(buf, sizeof(buf), stdin) == NULL)
+		       break;
+	       token = strtok(buf, " ");
+		j = 0;
+		while (token[j])
+		{
+			if (token[j] == '\n')
+			token[j] = '\0';
+			j++;
+		}
+
+		if (argv == NULL)
+		{
+			printf("memory allocation failed for argv\n");
+		}
+		i = 0;
+		while (token != NULL)
+		{
+			argv[i] = strdup(token);
+			if (argv[i] == NULL)
+				printf("memory allocation failed for argv[i]\n");
+			token = strtok(NULL, " ");
+			i++;
+		}
+
+	if (execve(argv[0], argv, environ) == -1)
+		printf("%s: No such file or directory\n", argv[0]);
+	printf("$ ");
+	fflush(stdout);
+	free(buf);
+	free(token);
+	i = 0;
+	while (argv[i])
+	{
+		free(argv[i]);
 		i++;
 	}
+	free(argv);
+	}
+	return (0);
 }
--- main.h
+++ main.h
@@ -2,13 +2,14 @@
 #define SH
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 extern char **environ;
 #define MAX_SIZE 1024
+#define MAX_WORDS 100
 /**
  * struct node - linked list for directories
  * @dir: directory
