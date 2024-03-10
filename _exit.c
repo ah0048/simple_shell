aa89aa1f9ff065bde9bc45fc25c301a7be41ba25
root <root@DESKTOP-2RRMLQV>
2024-03-10 12:05:54 +0200

    edit

--- _exit.c
+++ _exit.c
@@ -8,11 +8,18 @@ int isExit(char *value_str)
 {
 	int tmp = 0;
 
+	char *ptr;
+
 	if (!value_str)
 	{
 		return (0);
 	}
-	tmp = atoi(value_str);
+	tmp = strtol(value_str, &ptr, 10);
+	if (*ptr != '\0')
+	{
+		fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", value_str);
+		return (2);
+	}
 	if (tmp)
 	{
 		if (tmp >= 0)
